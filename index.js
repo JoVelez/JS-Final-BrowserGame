be0f8f26916f72f5e02b0f07d1ee091071e66e00
Joleena Velez <joleenavelez@gmail.com>
2021-12-11 10:37:39 -0800

    Added goals

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+List of Goals:
+- dungeons that randomize
+- floor sprites, walls can come later
+- randomized floor sprites for variety?
+- character should move
+- character should gain and lose health
+- enemies should lose health
+- enemy AI?
+- collectibles that add health
+- weapons pickup?
+- healthbars if possible
+*/
