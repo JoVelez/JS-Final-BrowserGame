03382730d5eab2198fcb22b57fdf5016a1a189f7
Joleena Velez <joleenavelez@gmail.com>
2021-12-15 19:39:58 -0800

    Created tile map, might need tile swap

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,14 @@
-import TileMap from "./Sprites.js";
+import TileMap from './Sprites.js';
 
-// sprite size
-const tileSize = 16;
+const tileSize = 32;
 const canvas = document.getElementById("gameCanvas");
-// returns a drawing context on the canvas
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 const tileMap = new TileMap(tileSize);
 
-// redraws screen every 'x' times 1 second
-function game(){
+function gameLoop(){
 tileMap.draw(ctx);
 
 }
 
-// the amount of times screen is redrawn per second
-setInterval(game, 1000 / 75);
+tileMap.setCanvasSize(canvas);
+setInterval(gameLoop, 1000 / 75);
--- src/Sprites.js
+++ src/Sprites.js
@@ -1,21 +1,110 @@
-// where sprites and scources are defined
+//exports to be used elsewhere
 export default class TileMap{
     constructor(tileSize){
         this.tileSize = tileSize;
 
-        this.coin = new Image()
-        this.coin.src = "../Images/Collect/coin.png"
+        this.coin = new Image();
+        this.coin.src = '../Images/Collect/tilewithcoin.png';
 
-        this.floortile1 = new Image()
-        this.floortile1.src = "../Images/DungeonTile/floortile1.png"
+        this.wall = new Image();
+        this.wall.src = '../Images/DungeonTile/walltile1.png';
+
+        this.innerBorder = new Image();
+        this.innerBorder.src = '../Images/DungeonTile/plainborder.png';
+
+        this.floor = new Image();
+        this.floor.src = '../Images/DungeonTile/floortile2.png';
     }
-    
-    // draws sprites to canvas
+
     map = [
-        [1,1,1]
-        [1,0,1]
-        [1,1,1]
-];
-    
-    draw(ctx) {}
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,3,3,3,3,3,0,3,3,3,3,3,0,3,3,3,3,3,0,1],
+        [1,0,0,0,3,0,0,0,0,0,3,0,0,0,0,0,3,0,0,0,1],
+        [1,1,1,0,3,0,3,0,0,0,3,0,0,0,3,0,3,0,1,1,1],
+        [1,1,1,0,0,0,3,0,3,0,3,0,3,0,3,0,0,0,1,1,1],
+        [1,1,1,0,3,3,3,0,3,0,0,0,3,0,3,3,3,0,1,1,1],
+        [1,1,1,0,3,3,3,0,3,3,3,3,3,0,3,3,3,0,1,1,1],
+        [1,0,0,0,0,0,3,0,0,0,0,0,0,0,3,0,0,0,0,0,1],
+        [1,0,3,3,3,0,3,0,1,1,2,1,1,0,3,0,3,3,3,0,1],
+        [1,0,0,0,3,0,3,0,1,2,2,2,1,0,3,0,3,0,0,0,1],
+        [1,1,1,0,3,0,0,0,1,2,2,2,1,0,0,0,3,0,1,1,1],
+        [1,1,1,0,3,0,0,0,1,2,2,2,1,0,0,0,3,0,1,1,1],
+        [1,1,1,0,3,0,3,0,1,1,1,1,1,0,3,0,3,0,1,1,1],
+        [1,1,1,0,0,0,3,0,0,0,0,0,0,0,3,0,0,0,1,1,1],
+        [1,1,1,0,3,3,3,3,3,0,3,0,3,3,3,3,3,0,1,1,1],
+        [1,1,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,1,1],
+        [1,1,0,3,3,3,0,3,0,3,3,3,0,3,0,3,3,3,0,1,1],
+        [1,1,0,0,0,3,0,3,0,0,0,0,0,3,0,0,0,3,0,1,1],
+        [1,1,0,3,0,3,0,3,0,3,3,3,0,3,0,3,0,3,0,1,1],
+        [1,1,0,3,0,0,0,3,0,0,3,0,0,3,0,3,0,0,0,1,1],
+        [1,1,0,3,3,3,0,3,3,0,3,0,3,3,0,3,3,3,0,1,1],
+        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        
+ ];
+
+ draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if(tile === 1){
+           this.#drawWall(ctx, column, row, this.tileSize);
+        }  else if (tile === 0) {
+            this.#drawCoin(ctx, column, row, this.tileSize);
+        }
+        else if (tile === 2) {
+            this.#drawfloor(ctx, column, row, this.tileSize);
+       }
+            else if (tile === 3) {
+            this.#drawinnerBorder(ctx, column, row, this.tileSize);
+       }
+      }
+     }
+    }
+
+    #drawWall(ctx, column, row, size) {
+        ctx.drawImage(
+          this.wall,
+          column * this.tileSize,
+          row * this.tileSize,
+          size,
+          size
+        );
+      }
+
+      #drawCoin(ctx, column, row, size) {
+        ctx.drawImage(
+          this.coin,
+          column * this.tileSize,
+          row * this.tileSize,
+          size,
+          size
+        );
+      }
+
+      #drawinnerBorder(ctx, column, row, size) {
+        ctx.drawImage(
+          this.innerBorder,
+          column * this.tileSize,
+          row * this.tileSize,
+          size,
+          size
+        );
+      }
+
+      #drawfloor(ctx, column, row, size) {
+        ctx.drawImage(
+          this.floor,
+          column * this.tileSize,
+          row * this.tileSize,
+          size,
+          size
+        );
+      }
+
+    setCanvasSize(canvas) {
+        canvas.width = this.map[0].length * this.tileSize;
+        canvas.height = this.map.length * this.tileSize;
+    }
 }
