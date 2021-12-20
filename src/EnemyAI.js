54159f8a348ffc5e11e14c7de633eaeea0614459
Joleena Velez <joleenavelez@gmail.com>
2021-12-19 20:48:09 -0800

    Adjusted file path

--- src/EnemyAI.js
+++ src/EnemyAI.js
@@ -1,16 +1,19 @@
 import TileMap from "./Sprites.js";
 import MovingDirection from "./MovementControls.js";
 
+// enemy class
 export default class Enemy {
  constructor(x,y,tileSize,velocity,tileMap){
-     this. x = x;
+     this.x = x;
      this.y = y;
      this.tileSize = tileSize;
      this.velocity = velocity;
      this.tileMap = tileMap;
 
+    // loads enemy images
      this.#loadImages();
 
+     // randomizes enemy movement
      this.movingDirection = Math.floor(
        Math.random() * Object.keys(MovingDirection).length
      );
@@ -18,52 +21,54 @@ export default class Enemy {
      this.directionTimerDefault = this.#random(1, 3);
      this.directionTimer = this.directionTimerDefault;
 
+     // power-up timer
      this.meatAboutToExpireTimerDefault = 10;
      this.meatAboutToExpireTimer = this.meatAboutToExpireTimerDefault;
-
-
  }
-    draw(ctx, pause, player){ 
-        if(!pause){
-            this.#move();
-            this.#changeDirection();
-        }
-        this.#setImage(ctx,player);
-    }
 
-    collideWith(player) {
-      const size = this.tileSize / 2;
-      if (
-        this.x < player.x + size &&
-        this.x + size > player.x &&
-        this.y < player.y + size &&
-        this.y + size > player.y
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+draw(ctx, pause, player){ 
+
+  // Stops enemy from moving when paused
+  if(!pause){
+    this.#move();
+    this.#changeDirection();
+  }
+    this.#setImage(ctx,player);
+}
+
+// Checks if enemy touches player
+collideWith(player) {
+  const size = this.tileSize / 2;
+  if 
+  ( this.x < player.x + size &&
+    this.x + size > player.x &&
+    this.y < player.y + size &&
+    this.y + size > player.y ) 
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
     
-    #setImage(ctx, player){
-      if (player.meatActive) {
-        this.#setImageWhenMeatActive(player);
-      } else {
-        this.image = this.slime;
-      }
-      ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
-      };
+#setImage(ctx, player){
+  if (player.meatActive) {
+  this.#setImageWhenMeatActive(player);
+  } else {
+  this.image = this.slime;
+}
+  ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+};
 
-      #setImageWhenMeatActive(player){
-        this.image = this.purpleslime;
-        if(player.meatAboutToExpire){
-          this.meatAboutToExpireTimer--;
-          if(this.meatAboutToExpireTimer === 0 ) {
-            this.meatAboutToExpireTimer = this.meatAboutToExpireTimerDefault;
-          }
-        }
-
-      }
+#setImageWhenMeatActive(player){
+  this.image = this.purpleslime;
+    if(player.meatAboutToExpire){
+  this.meatAboutToExpireTimer--;
+    if(this.meatAboutToExpireTimer === 0 ) {
+  this.meatAboutToExpireTimer = this.meatAboutToExpireTimerDefault;
+    }
+  }
+}
 
 
     #changeDirection() {
--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,23 @@
 import TileMap from './Sprites.js';
 
+// Map Size & Player Speed
 const tileSize = 38;
 const velocity = 2;
 
+// Canvas, map, ctx to draw to canvas, and player
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext('2d');
 const tileMap = new TileMap(tileSize);
 const player = tileMap.getPlayer(velocity);
 const enemies = tileMap.getEnemies(velocity);
 
+// Win & Lose 
 let gameOver = false;
 let gameWin = false;
 const gameOverSound = new Audio("../GameSound/lose.mp3");
 const gameWinSound = new Audio("../GameSound/win.mp3");
 
+// Main game loop, draws map, player, and enemies to screen
 function gameLoop(){
 tileMap.draw(ctx);-
 drawGameEnd();
@@ -23,6 +27,7 @@ checkGameOver();
 checkGameWin();
 }
 
+// Checks for win/loss
 function checkGameWin() {
     if (!gameWin) {
       gameWin = tileMap.didWin();
@@ -41,22 +46,26 @@ function checkGameWin() {
     }
   }
   
+  // Ends game if slimes collide w/ player
   function isGameOver() {
     return enemies.some(
       (enemy) => !player.meatActive && enemy.collideWith(player)
     );
   }
 
+  // Stops game until player moves, or after win/loss
 function pause() {
     return !player.madeFirstMove || gameOver || gameWin;
   }
 
+  // Displays "You Win" or "You Lose"
   function drawGameEnd() {
     if (gameOver || gameWin) {
       let text = " You Win!";
       if (gameOver) {
         text = "Game Over!";
       }
+
       ctx.fillStyle = "black";
       ctx.fillRect(0, canvas.height / 2.3, canvas.width, 80);
   
@@ -64,6 +73,7 @@ function pause() {
       ctx.fillStyle = "white";
       ctx.textAlign = "center";
       ctx.fillText(text, 400, canvas.height / 2);
+      
     }
   }
 
